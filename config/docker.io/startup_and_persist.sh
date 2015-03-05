189c0b10fbfc3008b65c7f7ff901167b6023a552
abetusk <abetusk@gmail.com>
2015-03-05 01:33:32 -0500

    WIP docker file

---
+++ config/docker.io/startup_and_persist.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rabbitmq-server -detached
+
+su -l genevieve -c " cd /home/genevieve/genevieve && \
+  workon genevieve && \
+  python manage.py runserver 0.0.0.0:8000 " &
+
+
+su -l genevieve -c " cd /home/genevieve/genevieve && \
+  workon genevieve && \
+  celery -A genevieve worker -l info & "
+
+
+while [ true ]
+do
+  echo ping
+  sleep 3
+done
